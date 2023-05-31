ca917f1d5f81bda1ca896fb48e31c099416e9f87
Conor <conor.lowney@digital.cabinet-office.gov.uk>
2023-05-31 15:48:00 +0100

    add metadata to manual header

--- app/presenters/content_item/multi_part.rb
+++ app/presenters/content_item/multi_part.rb
@@ -37,21 +37,6 @@ module ContentItem
       details["body"]
     end
 
-    private
-
-    def other_metadata
-      updated_metadata(public_updated_at)
-    end
-
-    def updated_metadata(updated_at)
-      updates_link = view_context.link_to(I18n.t("manuals.see_all_updates"), "#{base_path}/updates")
-      { I18n.t("manuals.updated") => "#{display_date(updated_at)}, #{updates_link}" }
-    end
-
-    def details
-      content_item["details"]
-    end
-
     def multi_part_metadata
       {
         from:,
@@ -60,5 +45,22 @@ module ContentItem
         inverse: true,
       }
     end
+
+    private
+
+    def other_metadata
+      updated_metadata(public_updated_at)
+    end
+
+    def updated_metadata(updated_at)
+      updates_link = view_context.link_to("See all updates", "#{base_path}/updates")
+      { I18n.t("manuals.updated") => "#{display_date(updated_at)}, #{updates_link}" }
+    end
+
+    def details
+      content_item["details"]
+    end
+
+
   end
 end
